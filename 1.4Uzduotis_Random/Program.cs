3c729539dc7525b128668c61339b172b3ea56bfe
Laura <Laura@DESKTOP-BESSB6E>
2023-09-27 00:05:01 +0300

    Add project files.

---
+++ 1.4Uzduotis_Random/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1._4Uzduotis_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (int i in ReturnNumSum())
+            {
+            Console.WriteLine(i);
+            }
+         
+
+        }
+        public static List<int> ReturnNumSum()
+        {
+            Random random = new Random();
+
+            List<int> list = new List<int>();
+
+
+            for (int i = 0; i < 101; i++)
+            {
+                list.Add(random.Next(1, 7) + random.Next(1, 7));
+            }
+
+            return list;
+        }
+    }
+}
